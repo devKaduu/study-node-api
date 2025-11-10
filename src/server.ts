fab9451b9a376c27bcd7684a274d363d05d8369a
Carlos Eduardo <kadufenix10@gmail.com>
2025-11-10 20:00:50 -0300

    feat: configurando dockerfile

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { server } from "./app.ts";
 
 // Run the server!
-server.listen({ port: 3000 }).then(() => {
+server.listen({ port: 3000, host: "0.0.0.0" }).then(() => {
   console.log("Server is running at http://localhost:3000");
 });
